28a1726060f49539a3999d63c9350cf8ecda7147
Jeff Prestes <jeffprestes@gmail.com>
2018-10-11 21:42:48 -0300

    Ajustes na pagina

--- aula07/script.js
+++ aula07/script.js
@@ -1,8 +1,8 @@
 function computarVoto() {
     var resumoVoto;
-    resumoVoto = "O seu voto foi <br>"
-    resumoVoto = resumoVoto + "Numero de Cotas a serem aumentadas sugeridos por você foi: " + document.formVotacao.nroCotas.value + "<br>";
-    if (document.formVotacao.nroCotas.value == "sim") {
+    resumoVoto = "O seu voto foi <br>";
+    resumoVoto = resumoVoto + "cotas: " + document.formVotacao.nroCotas.value + "<br>";
+    if (document.formVotacao.pauta1.value == "sim") {
         resumoVoto = resumoVoto + "Voce concordou com a Pauta1";
     } else {
         resumoVoto = resumoVoto + "Voce não concordou com a Pauta1";
