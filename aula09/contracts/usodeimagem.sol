e70c3939d2297cf878fa2f25f356a6756b65a241
Jeff Prestes <jeffprestes@gmail.com>
2018-10-30 16:35:58 -0300

    Realiza envio de pagamento a agente

--- aula09/contracts/usodeimagem.sol
+++ aula09/contracts/usodeimagem.sol
@@ -2,18 +2,33 @@ pragma solidity 0.4.25;
 
 contract UsoDeImagem {
 	
-	string public nomeEmpresa;
+    string public nomeEmpresa;
+    address agente;
+    address artista;
 	
-	constructor() public {
-    	nomeEmpresa = "Artista SuperPop Ltda";
-	}
+    modifier somenteArtista() {
+        require(msg.sender==artista, "Somente artista pode realizar essa operação");
+        _;
+    }
+
+    constructor() public {
+        nomeEmpresa = "Artista SuperPop Ltda";
+        artista = msg.sender;
+    }
 	
-	function definirNomeDaEmpresa(string qualNomeDaEmpresa) public {
-    	nomeEmpresa = qualNomeDaEmpresa;
-	}
+    function definirNomeDaEmpresa(string qualNomeDaEmpresa) public {
+        nomeEmpresa = qualNomeDaEmpresa;
+    }
+
+    function definirAgente(address qualAgente) public somenteArtista  {
+        require(qualAgente != address(0), "Endereço de agente invalido");
+        agente = qualAgente;
+    }
 	
-	function receberPeloUso() public payable {
+    function receberPeloUso() public payable {
         require(msg.value >= 100 szabo, "Por favor pague o valor mínimo");
-	}
-	
+        if (agente != address(0)) {
+            agente.transfer((msg.value * 10) / 100);
+        }
+    }
 }
