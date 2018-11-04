df891b18ef3158406ccb09bf23f779691b38e3bc
Jeff Prestes <jeffprestes@gmail.com>
2018-11-04 11:08:13 -0200

    Adiciona pagina e conexao ao contrato de uso de imagem

---
+++ aula11/js/conexaoweb3.js
@@ -0,0 +1,47 @@
+var conta;
+//Checa a cada 2 segundos se a conta Ethereum foi alterada no Metamask
+$(document).ready(async () => {
+    setInterval(() => {
+        if (web3.eth.accounts[0] !== this.conta) {
+            conta = web3.eth.accounts[0];
+        }
+        checkWeb3();
+    }, 2000)
+});
+
+window.addEventListener('load', async (event) => {
+    // Navegadores com novo Metamask    
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // Solicita acesso a carteira Ethereum se necessário
+            await ethereum.enable()
+            console.log("Usando nova versão");
+            // Contas agora estão expostas                  
+        } catch (error) { // Usuário ainda não deu permissão para acessar a carteira Ethereum        
+            alert('Por favor, dê permissão para acessarmos a sua carteira Ethereum.');
+            $('#statusConexao').text('Desconectado');
+        }
+    } else if (window.web3) { // Navegadores DApp antigos
+        window.web3 = new Web3(web3.currentProvider)
+    } else { // 
+        alert('Para utilizar os nossos serviços você precisa instalar o Metamask. Por favor, visite: metamask.io');
+    }
+    checkWeb3()
+});
+
+// Verifica a conexão Web3
+function checkWeb3() {
+    // Verifica o status da conexão 
+    if (web3 && web3.isConnected()) {
+        if (web3.eth.accounts[0] == undefined)  {
+            console.info('Não esta conectado ao Metamask');
+            $('#statusConexao').text('Desconectado');  
+        }  else {
+            console.info('Conectado. Qual versão da lib Web3 foi injetado pelo Metamask? ' + web3.version.api);
+            $('#statusConexao').text('Conectado a conta ' + web3.eth.accounts[0]);  
+        }
+    } else {
+        $('#statusConexao').text('Desconectado');
+    }
+}
---
+++ aula11/js/usodeimagem.js
@@ -0,0 +1,72 @@
+const contratoUsoDeImagemABI = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "nomeEmpresa",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "receberPeloUso",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "qualAgente",
+				"type": "address"
+			}
+		],
+		"name": "definirAgente",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "qualNomeDaEmpresa",
+				"type": "string"
+			}
+		],
+		"name": "definirNomeDaEmpresa",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+];
+
+contratoUsoDeImagem = new window.web3.eth.Contract(contratoUsoDeImagemABI, "0xcb5eb5a839b07d97faf2103c133e5f078123d5e7");
+
+function obtemNomeEmpresa() {
+    contratoUsoDeImagem.methods.nomeEmpresa.call({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.error(err);
+        } else {
+            let objStatus = document.getElementById("spanNomeEmpresa");
+            objStatus.innerText = resultado;
+        }
+    });
+}
