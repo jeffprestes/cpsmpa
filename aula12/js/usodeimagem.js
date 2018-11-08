07c7d3f28f167b0c398994a4a437cdc09e669107
Jeff Prestes <jeffprestes@gmail.com>
2018-11-08 16:01:28 -0200

    Melhora o retorno visual

--- aula12/js/usodeimagem.js
+++ aula12/js/usodeimagem.js
@@ -86,7 +86,7 @@ function registrarNomeEmpresa() {
         } else {
             console.log("Resultado");
             console.log(resultado);
-            statusTransacao.innerHTML = "Sucesso: " + resultado;
+            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
         }
     });
 }
